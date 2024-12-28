8a8c939b1892ab7365421194f6a8ce1f4067e8f1
Seoyeon99 <82697009+baekseoyeon@users.noreply.github.com>
2024-12-28 21:25:16 +0900

    Title: 간단한 사칙연산, Time: 42ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 241228/간단한 사칙연산/simple-arithmetic-operation.py
@@ -0,0 +1,11 @@
+def Cal():
+    num=input()
+    num_list=num.split(" ")
+    a = int(num_list[0])
+    b = int(num_list[1])
+    print(a+b)
+    print(a-b)
+    print(a//b)
+    print(a%b)
+
+Cal()
